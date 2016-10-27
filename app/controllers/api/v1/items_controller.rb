1c2569c017b4ce0d41d04383074449c24c087a79
Alex Benoit <alexander.benoit@outlook.com>
2016-10-27 19:56:38 +0200

    started show

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ItemsController < Api::V1::BaseController
     @items = Item.all
     # @items = policy_scope(Item)
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  # private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # API
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show]
     end
   end
 end
